ce929e09226165095c0e5efffd022590e54e307a
sjohn65189 <70227912+sjohn65189@users.noreply.github.com>
2024-04-02 20:50:47 -0500

    added script for Button

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+    private Light postLight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        postLight = GetComponent<Light>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Button"))
+        {
+            postLight.enabled = true;
+        }
     }
 }
