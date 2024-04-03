973f22b011f9fc4d273e91a3cc72337e3316bfd7
Masonkta <88817850+Masonkta@users.noreply.github.com>
2024-04-02 20:37:49 -0500

    Timer script now works

--- Assets/Custom Scripts/Timer.cs
+++ Assets/Custom Scripts/Timer.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
+using UnityEngine.XR;
+using System;
+
+public enum TimerStateType
+{
+    On,
+    Off,
+}
 
 public class Timer : MonoBehaviour
 {
+    public TimerStateType State { get; private set; }
+    private TextMeshPro txt;
+    TimeSpan CurrentTime;
+    TimeSpan startingTime;
+
     // Start is called before the first frame update
     void Start()
     {
+        CurrentTime = new(0, 30, 0);
+        State = TimerStateType.On;
+        txt = GetComponent<TextMeshPro>();
         
+    }
+
+    public void TurnOn()
+    {
+        State = TimerStateType.On;
+    }
+
+    public void TurnOff() {
+        State = TimerStateType.Off;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (State)
+        {
+            case TimerStateType.Off:
+                break;
+
+            case TimerStateType.On:
+                txt.text = CurrentTime.ToString(@"mm\:ss");
+                CurrentTime = CurrentTime.Subtract(TimeSpan.FromSeconds(Time.deltaTime));
+                break;
+        }
     }
 }
