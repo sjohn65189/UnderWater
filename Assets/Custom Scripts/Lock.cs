c34a584be6f9c53fda1f7ee621f09a2ed0167a5c
Masonkta <88817850+Masonkta@users.noreply.github.com>
2024-04-09 21:25:49 -0500

    Setting Door into main scene

--- Assets/Custom Scripts/Lock.cs
+++ Assets/Custom Scripts/Lock.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Lock : MonoBehaviour
 {
+    public GameObject Door2;
     public GameObject Door;
     // Start is called before the first frame update
     void Start()
@@ -14,5 +17,6 @@ public class Lock : MonoBehaviour
     public void Open()
     {
         Door.SetActive(false);
+        Door2.SetActive(false);
     }
 }
